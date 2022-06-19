980ee04770062bbfb3d533fafeca5600030a83ad
riisatoro <riisa.toro@gmail.com>
2022-06-19 16:44:47 +0300

    Remove unused files

--- include/SDL2/SDL_revision.h
+++
@@ -1,2 +0,0 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL@eb7e29a8b2b6cda62d050396419b19b5d72e52cf"
-#define SDL_REVISION_NUMBER 0
---
+++ src/include/defines.h
@@ -0,0 +1,3 @@
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 500
+#define SCREEN_TITLE "Roguelike"
